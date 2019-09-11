1014c6e2704494f52bc392dad80122a2c301f522
Todd Smitala <tc.smitala@gmail.com>
2019-09-10 20:41:18 -0400

    Created form with submit and linked caesar code to rotate

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- create my form here -->
+    <form action="/" method="post">
+        <label for="rot">
+            Rotate by:
+            <input type="text" name="rot" value="0"/>
+        </label>
+        <textarea name="text"></textarea>
+        <input type="submit" value="Submit Query"/>
+    </form>
+
+    </body>
+</html>                
+"""
+
+@app.route("/", methods=['POST'])
+def encrypt():
+ text = str(request.form['text'])
+ rot = int(request.form['rot'])
+ rotated = rotate_string(text,rot)
+ return "<h1>"+str(rotated)+"</h1>"
+
+@app.route("/")
+def index():
+    return form
+
+app.run()    
